513080fc74590fdc4ef110534f2082f15735ba7f
dawneraq <dawneraq@gmail.com>
2021-02-20 16:51:16 -0800

    add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,34 @@ module.exports = {
   purge: ['./src/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: [
+        'Actor',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji',
+      ],
+      serif: [
+        'Palatino',
+        'ui-serif',
+        'Georgia',
+        'Cambria',
+        'Times New Roman',
+        'Times',
+        'serif',
+      ],
+    },
     extend: {},
   },
   variants: {
